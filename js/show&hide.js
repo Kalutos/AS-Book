b3d9ff5b971d80d58ede953162340df2c969f631
Kalutos <arthurslfpro@gmail.com>
2024-05-14 04:49:49 +0200

    je vais pleurer

---
+++ js/show&hide.js
@@ -0,0 +1,7 @@
+document.getElementById('showMenu').addEventListener('click', function() {
+    document.querySelector('.menu').style.display = 'flex';
+});
+
+document.getElementById('hideMenu').addEventListener('click', function() {
+    document.querySelector('.menu').style.display = 'none';
+});
